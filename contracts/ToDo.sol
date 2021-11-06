529baa1bb837b03ceb3ce33d26de5a862fc6bb6f
Nanak <nanakbandyopadhyay@hotmail.com>
2021-11-06 11:19:50 +0530

    add server-config and createTask function in ToDo.sol

--- contracts/ToDo.sol
+++ contracts/ToDo.sol
@@ -2,5 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ToDo {
-    uint public taskCount = 0;
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Be a blockchain developer");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
